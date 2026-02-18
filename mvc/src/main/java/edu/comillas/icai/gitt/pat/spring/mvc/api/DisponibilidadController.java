0c0a91984d3b62c5cf5914fe0f4815564ca61298
i-gh-OS <202301839@alu.comillas.edu>
2026-02-18 13:06:34 +0100

    404 si no existe la pista pedida

--- mvc/src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/api/DisponibilidadController.java
+++ mvc/src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/api/DisponibilidadController.java
@@ -1,6 +1,7 @@
 package edu.comillas.icai.gitt.pat.spring.mvc.api;
 
 import edu.comillas.icai.gitt.pat.spring.mvc.records.Disponibilidad;
+import edu.comillas.icai.gitt.pat.spring.mvc.data.AlmacenDatos;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -58,10 +59,7 @@ public class DisponibilidadController {
             return ResponseEntity.badRequest().build();
         }
 
-        // ToDo: aquí más adelante irás a PistaRepository/Service para comprobar existencia real
-        boolean pistaExiste = true; // placeholder por ahora
-
-        if (!pistaExiste) {
+        if (!AlmacenDatos.PISTAS.containsKey(courtId)) {
             return ResponseEntity.notFound().build(); // 404
         }
 
