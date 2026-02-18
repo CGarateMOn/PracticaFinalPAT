0f1e8a5011561f431b3a4652094925869e94fb2a
i-gh-OS <202301839@alu.comillas.edu>
2026-02-18 13:18:32 +0100

    arreglo idPista null en /availability
    antes si no especificaban court en /availability salia un Disponibilidad con idPista=null.
    Ahora:
    si courtId lo especifican --> devuelve Disponibilidad de esa pista
    si no --> devuelve List<Disponibilidad> (una por pista)

--- mvc/src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/api/DisponibilidadController.java
+++ mvc/src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/api/DisponibilidadController.java
@@ -11,6 +11,9 @@ import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.List;
 
+import static java.util.Collections.emptyList;
+import static org.springframework.http.ResponseEntity.ok;
+
 @RestController
 @RequestMapping("/pistaPadel")
 public class DisponibilidadController {
@@ -32,15 +35,19 @@ public class DisponibilidadController {
             return ResponseEntity.badRequest().build(); // 400
         }
 
-        logger.info("Consulta disponibilidad fecha {} courtId {}", localDate, courtId);
+        List<Disponibilidad> result;
+        if (courtId != null) {
+            if (!AlmacenDatos.PISTAS.containsKey(courtId)) return ok(emptyList());
+            result = List.of(new Disponibilidad(courtId, localDate, List.of()));
+            // de momento ponemos List.of() porque todavia no tenemos los tramos de tiempo reales
+            // cuando implementemos la logica real pondremos el resultado de eso
+        } else {
+            result = AlmacenDatos.PISTAS.keySet().stream()
+                    .map(id -> new Disponibilidad(id, localDate, List.of()))
+                    .toList();
+        }
+        return ResponseEntity.ok(result);
 
-        Disponibilidad disponibilidad = new Disponibilidad(
-                courtId,
-                localDate,
-                List.of()
-        );
-
-        return ResponseEntity.ok(disponibilidad);
     }
 
     @GetMapping("/courts/{courtId}/availability")
