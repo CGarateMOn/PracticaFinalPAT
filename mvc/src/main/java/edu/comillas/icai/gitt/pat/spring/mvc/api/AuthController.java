be2b5da21b01e5a67251faf69193038842a38fea
MartinaIscarSoto <martina.iscar05@gmail.com>
2026-02-18 09:46:49 +0100

    AuthController. Funcionan los errores

--- mvc/src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/api/AuthController.java
+++ mvc/src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/api/AuthController.java
@@ -2,42 +2,23 @@ package edu.comillas.icai.gitt.pat.spring.mvc.api;
 
 import edu.comillas.icai.gitt.pat.spring.mvc.records.ModeloUsuarioIncorrecto;
 import edu.comillas.icai.gitt.pat.spring.mvc.records.Usuario;
-import edu.comillas.icai.gitt.pat.spring.mvc.seguridad.ExcepcionUsuarioIncorrecto;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.ui.Model;
+
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import static edu.comillas.icai.gitt.pat.spring.mvc.data.AlmacenDatos.USUARIOS;
-import static java.util.stream.Collectors.toList;
+import static edu.comillas.icai.gitt.pat.spring.mvc.data.AlmacenDatos.usuarios;
 
 @RestController
 @RequestMapping("/pistaPadel/auth")
 public class AuthController {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
-
-
-
-//    @ExceptionHandler(ExcepcionUsuarioIncorrecto.class)
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    public List<ModeloUsuarioIncorrecto> usuarioIncorrecto(ExcepcionUsuarioIncorrecto ex) {
-//        return ex.getErrores().stream().map(error -> new ModeloUsuarioIncorrecto(
-//                error.getDefaultMessage(), error.getField(), error.getRejectedValue()
-//        )).toList();
-//    }
 
     @PostMapping("/register")
     public ResponseEntity crea(
@@ -55,13 +36,13 @@ public class AuthController {
                     .toList();
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(usuarioIncorrecto);
         }
-        for(Usuario u : USUARIOS.values() ){
+        for(Usuario u : usuarios.values() ){
             if(u.email().equals(usuario.email())){
                 return ResponseEntity.status(HttpStatus.CONFLICT).body("El email ya existe");
             }
         }
         logger.info("Gracias", usuario.nombre(), "tu cuenta se ha creado correctamente");
-        USUARIOS.put(usuario.idUsuario(), usuario);
+        usuarios.put(usuario.idUsuario(), usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
     }
 
